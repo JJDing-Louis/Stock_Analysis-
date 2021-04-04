b462f467de2c5f907c520eb93f98d992ff0202a2
J.J.Ding <louis7012161@gmail.com>
2021-04-04 16:39:38 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> complete file reading

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,31 @@ namespace Stock_Analysis
     public partial class Form1 : Form
     {
         OpenFileDialog odf = new OpenFileDialog();
+        String fileAdress;
 
         public Form1()
         {
             InitializeComponent();
+            //初始化OpenFileDialog
+            odf.AddExtension = true;
+            odf.CheckFileExists = true;
+            odf.Filter = "csv檔|*.csv|純文字檔|*.txt|所有檔案|*.*";
+            odf.FilterIndex = 0;
+            odf.FileName = String.Empty;
+            odf.InitialDirectory = @"C:\";
+            odf.Multiselect = false;
+            odf.RestoreDirectory = true;
+
+            odf.ShowReadOnly = true;
+            odf.Title = "請選取股票資料";
+            odf.FileOk += new CancelEventHandler(odf_FileOk);
+        }
+
+        private void odf_FileOk(object sender, CancelEventArgs e)
+        {
+            //MessageBox.Show(odf.FileName);
+            txtfile_address.Text = odf.FileName;
+            
 
         }
 
