093c97ca1654388b403f1b9f53ff52e6030f4dd6
J.J.Ding <louis7012161@gmail.com>
2021-04-04 15:36:27 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Add UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,42 @@ namespace Stock_Analysis
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtfile_address = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtfile_address
+            // 
+            this.txtfile_address.Location = new System.Drawing.Point(12, 12);
+            this.txtfile_address.Name = "txtfile_address";
+            this.txtfile_address.Size = new System.Drawing.Size(661, 22);
+            this.txtfile_address.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 40);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(661, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1109, 584);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtfile_address);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtfile_address;
+        private System.Windows.Forms.TextBox txtSearch;
     }
 }
 
