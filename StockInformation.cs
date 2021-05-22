ee599c6da8ab0f7babedb33be7381ef6f0697386
J.J.Ding <louis7012161@gmail.com>
2021-05-22 20:40:30 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> 新增StockInformation Class

---
+++ StockInformation.cs
@@ -0,0 +1,24 @@
+﻿namespace Stock_Analysis
+{
+    internal class StockInformation
+    {
+        public string StockID { get; }
+        public string StockName { get; }
+        public int BuyTotal { get; }
+        public int CellTotal { get; }
+        public double AvgPrice { get; }
+        public int BuyCellOver { get; }
+        public int SecBrokerCnt { get; }
+
+        public StockInformation(string stockID, string stockName, int buyTotal, int cellTotal, double avgPrice, int buyCellOver, int secBrokerCnt)
+        {
+            StockID = stockID;
+            StockName = stockName;
+            BuyTotal = buyTotal;
+            CellTotal = cellTotal;
+            AvgPrice = avgPrice;
+            BuyCellOver = buyCellOver;
+            SecBrokerCnt = secBrokerCnt;
+        }
+    }
+}
